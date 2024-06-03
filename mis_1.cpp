c1f12b3f8b0374d35c767ed985be01a017d98632
DGHaZe <34streets@gmail.com>
2024-06-03 10:02:57 +0300

    первый вариант инкримента

--- mis_1.cpp
+++ mis_1.cpp
@@ -12,21 +12,26 @@ class SimpleCircle{
     double GetRadius() const { return itsRadius; } // мет. получения радиуса
     void SetRadius(double r) { itsRadius = r; } // мет. присвоения радиуса
 
+    void operator++() { ++itsRadius; }
+
     private:
     double itsRadius; // переенная со значение радиуса
+
+    int itsValue; // переме. для хранения значения инкримента
 };
 
 //иницалализация конструкторов
-SimpleCircle::SimpleCircle():itsRadius(0){};
+SimpleCircle::SimpleCircle():itsRadius(0), itsValue(0){};
 SimpleCircle::~SimpleCircle(){};
 
 int main(){
-    /*Код для проверки работы класса
+
+    setlocale(LC_ALL, "Russian");
+
     SimpleCircle myCircle;
-    cout << "myCircle radius: " << myCircle.GetRadius() << endl;
-    myCircle.SetRadius(15);
-    cout << "myCircle radius: " << myCircle.GetRadius() << endl;
-    */
-    //
+    cout << "Значение itsRadius: " << myCircle.GetRadius() << endl;
+    ++myCircle;
+    cout << "Значение itsRadius: " << myCircle.GetRadius() << endl;
+
     return 0;
 }
