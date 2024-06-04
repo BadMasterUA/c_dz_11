5cf443bbbcd5cc2d137b1a998657fbfb096c4c68
DGHaZe <34streets@gmail.com>
2024-06-04 13:18:25 +0300

    add files

--- mis_3_4.cpp
+++ mis_3_4.cpp
@@ -7,41 +7,34 @@ class SimpleCircle
 {
 
 public:
-    SimpleCircle();  // конструктор
-    SimpleCircle(const SimpleCircle &);
-    ~SimpleCircle(); // деконструуктор
+    SimpleCircle();                     // конструктор
+    SimpleCircle(const SimpleCircle &); // конструктор копирования
+    ~SimpleCircle();                    // деконструуктор
 
     double GetRadius() const { return *itsRadius; } // мет. получения радиуса
     void SetRadius(double r) { *itsRadius = r; }    // мет. присвоения радиуса
 
     const SimpleCircle &operator++();   // меняем void, что бы возвращался объект
     const SimpleCircle operator++(int); // пост инкримент с целым параметром
-    SimpleCircle& operator=(const SimpleCircle &);
+    SimpleCircle &operator=(const SimpleCircle &);
 
 private:
     double *itsRadius; // переенная со значение радиуса
 
-    int itsValue; // переме. для хранения значения инкримента
+    // int itsValue; // переме. для хранения значения инкримента
 };
 
 // иницалализация конструкторов
 SimpleCircle::SimpleCircle()
 {
-    itsRadius = new double(0);
+    itsRadius = new double(5);
 };
 
+// конструктор копий
 SimpleCircle::SimpleCircle(const SimpleCircle &rhs)
 {
-    int val = rhs.GetRadius();
-    itsRadius = new double(val);
-}
-
-SimpleCircle& SimpleCircle::operator=(const SimpleCircle &rhs)
-{
-    if (this == &rhs)
-        return *this;
+    itsRadius = new double;
     *itsRadius = rhs.GetRadius();
-    return *this;
 }
 
 SimpleCircle::~SimpleCircle()
@@ -64,20 +57,31 @@ const SimpleCircle SimpleCircle::operator++(int)
     return temp;
 }
 
+SimpleCircle& SimpleCircle::operator=(const SimpleCircle &rhs)
+{
+    if (this == &rhs)
+        return *this;
+    *itsRadius = rhs.GetRadius();
+    return *this;
+}
+
 int main()
 {
+
+    setlocale(LC_ALL, "Russian");
+
     SimpleCircle myCircle;
-    cout << "itsRadius: " << myCircle.GetRadius() << endl;
+    cout << "Значение itsRadius: " << myCircle.GetRadius() << endl;
     ++myCircle;
-    cout << "itsRadius: " << myCircle.GetRadius() << endl;
+    cout << "Значение itsRadius: " << myCircle.GetRadius() << endl;
 
     SimpleCircle a = ++myCircle;
-    cout << "itsRadius a: " << a.GetRadius();
-    cout << " and myCircle: " << myCircle.GetRadius() << endl;
+    cout << "Значение itsRadius a: " << a.GetRadius();
+    cout << " и myCircle: " << myCircle.GetRadius() << endl;
 
     a = myCircle++;
-    cout << "itsRadius a: " << a.GetRadius();
-    cout << " and myCircle: " << myCircle.GetRadius() << endl;
+    cout << "Значение itsRadius a: " << a.GetRadius();
+    cout << " и myCircle: " << myCircle.GetRadius() << endl;
 
     return 0;
 }
